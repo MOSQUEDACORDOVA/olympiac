0f4306b7990874165ca5407c7ff17882342454fd
MOSQUEDACORDOVA <elmosquedacordova@gmail.com>
2022-04-19 17:44:17 -0500

    test

--- app-assets/js/scripts/pages/page-blog-edit-nutricion1.js
+++ app-assets/js/scripts/pages/page-blog-edit-nutricion1.js
@@ -3,7 +3,7 @@
 	Description: Blog edit field select2 and quill editor JS
 	----------------------------------------------------------------------------------------
 	Item Name: Olympiac  - Vuejs, HTML & Laravel Admin Dashboard Template
-	Author: PIXINVENT
+	Author: PIXINVENTjjjjjj
   Author URL: http://www.themeforest.net/user/pixinvent
 ==========================================================================================*/
 (function (window, document, $) {
